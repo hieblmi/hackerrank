f69f55d6e8a171470d77c397ce5c59e1112f53fc
Michael Hiebl <Michael.Hiebl@AlticeUSA.com>
2017-03-10 18:14:23 -0500

    StringConstruction

--- src/com/hackerrank/algorithms/strings/StringConstruction.java
+++ src/com/hackerrank/algorithms/strings/StringConstruction.java
@@ -1,7 +1,23 @@
 package com.hackerrank.algorithms.strings;
 
-/**
- * Created by mhiebl on 3/10/2017.
- */
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class StringConstruction {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int a0 = 0; a0 < n; a0++) {
+            String s = in.next();
+            String p = "";
+            int cost = 0;
+            Set<Character> set = new HashSet<>();
+            for (int i = 0; i < s.length(); i++) {
+                set.add(s.charAt(i));
+            }
+            System.out.println(set.size());
+        }
+    }
 }
